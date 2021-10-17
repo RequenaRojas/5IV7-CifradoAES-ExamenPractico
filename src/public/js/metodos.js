12eda063824b954e96177b6aefcfc454451a8a79
RequenaRojas <mrequenar1900@alumno.ipn.mx>
2021-10-17 03:36:06 -0500

    listoooooooooooooo

--- src/public/js/metodos.js
+++ src/public/js/metodos.js
@@ -1,25 +1,34 @@
-
-
-
+var mensaje;
 function leerArchivo(e) {
   const archivo = e.target.files[0];
-  if (!archivo) {
-    return;
-  }
   const lector = new FileReader();
-    lector.onload = function(e) {
-    mensaje = e.target.result;
 
-
+  lector.onload = function(e) {
+    const textoplano = e.target.result;
+    mensaje = textoplano;
   };
   lector.readAsText(archivo);
-  return mensaje;
 }
 
-function escribirFichero(cifrado , nombre){
+
+document.querySelector('#fichero').addEventListener('change', leerArchivo, false);
+
+
+
+function escribirFichero(cifrado ,isCifrado){
 
   const elm = document.createElement("a");
-  blob = new Blob([cifrado.toString(CryptoJS.enc.Utf8)], {type: "octet/stream"});
+  var blob;
+  var nombre;
+  
+  if(isCifrado){
+    blob = new Blob([cifrado], {type: "octet/stream"});
+    nombre = "textoCifrado";
+
+  }else{
+    blob = new Blob([cifrado.toString(CryptoJS.enc.Utf8)], {type: "octet/stream"});
+    nombre = "textoDescifrado";
+  }
 
   url = window.URL.createObjectURL(blob);
   elm.href = url;
@@ -30,9 +39,13 @@ function escribirFichero(cifrado , nombre){
 }
       
  function encriptar () {
-  if(document.getElementById('archivo').value!=""){ 
 
-    if(/(.txt)$/i.exec(document.getElementById('fichero').value)){  
+  const fichero = document.querySelector('#fichero');
+
+
+  if(fichero.value!=""){ 
+
+    if(/(.txt)$/i.exec(fichero.value)){  
 
       const tipo = document.getElementById('combobox').value;
       var password = document.getElementById('password').value;
@@ -43,12 +56,20 @@ function escribirFichero(cifrado , nombre){
       if( (tipo == '128' && String(password).length == 16 ) || (tipo == '192' && String(password).length == 24)
           || (tipo == '256' && String(password).length == 32) ){
             
-            const cifrado = CryptoJS.AES.crypt(leerArchivo(), Password);
-            escribirFichero(cifrado , 'MensajeEncriptado');
+            const cifrado = CryptoJS.AES.encrypt(mensaje, password);
+            escribirFichero(cifrado , true);
 
       }else{
-          const numdig = tipo/8
-          alert('la contraseña no cumple con el formato de '+ tipo + ' bits, Osea no tiene '+ numdig+ 'simbolos');
+
+          if(tipo == '0'){
+
+            alert('Debe de seleccionar un formato para la contraseña')
+
+          }else{
+            const numdig = tipo/8
+            alert('la contraseña no cumple con el formato de '+ tipo + ' bits (' +numdig+ ' digitos), en vez, cuenta con '+ String(password).length + ' simbolos');
+          }
+          
       }
     }else{
       alert('Solo se aceptan archivos *.txt')
@@ -60,25 +81,24 @@ function escribirFichero(cifrado , nombre){
 };
 
 function desencriptar () {
-    if(document.getElementById('fichero').value!=""){ 
+  const fichero = document.querySelector('#fichero');
+    if(fichero.value!=""){ 
 
-      if(/(.txt)$/i.exec(document.getElementById('fichero').value)){  
-
+      if(/(.txt)$/i.exec(fichero.value)){  
         const tipo = document.getElementById('combobox').value;
         var password = document.getElementById('password').value;
 
-        
 
         // Si la contraseña cumple con el requerimiento del combobox se procedera a escribir el fichero
         if( (tipo == '128' && String(password).length == 16 ) || (tipo == '192' && String(password).length == 24)
           	|| (tipo == '256' && String(password).length == 32) ){
               
-              const cifrado = CryptoJS.AES.decrypt(leerArchivo(), Password);
-              escribirFichero(cifrado, 'MensajeDesencriptado');
+              const cifrado = CryptoJS.AES.decrypt(mensaje, password);
+              escribirFichero(cifrado, false);
 
         }else{
             const numdig = tipo/8
-            alert('la contraseña no cumple con el formato de '+ tipo + ' bits, Osea no tiene '+ numdig+ 'simbolos');
+            alert('la contraseña no cumple con el formato de '+ tipo + ' bits (' +numdig+ ' digitos), en vez, cuenta con '+ String(password).length + ' simbolos');
         }
       }else{
         alert('Solo se aceptan archivos *.txt')
@@ -89,7 +109,6 @@ function desencriptar () {
     }
 };
 
-document.querySelector('#fichero').addEventListener('change', leerArchivo, false);
 
   
 
